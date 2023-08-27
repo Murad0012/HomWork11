87c0cd0b7e0f099ef128fd4df06d3ce66acfe98e
Predator <murad.mammedzade11@gmail.com>
2023-08-27 07:01:22 -0700

    save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "java";
+            input = input.ToUpper();
+            char[] charArray = input.ToCharArray();
+            char temp;
+            for (int i = 0; i < charArray.Length - i; i++)
+            {
+                temp = charArray[i];
+                charArray[i] = charArray[input.Length - i - 1];
+                charArray[input.Length - i - 1] = temp;
+            }
+            Console.WriteLine(charArray);
+        }
+    }
+}
